6639469bff953d1810469241a64e50b1e2f3b742
Safiqur_Rahman <safiqurrahmang7@gmail.com>
2025-01-02 21:12:46 +0530

    Improving the NLP tasks

--- src/save_model.py
+++
@@ -1,9 +0,0 @@
-import tensorflow as tf
-
-def save_trained_model(model, model_path):
-    """Save the trained model to the specified path."""
-    model.save(model_path)
-    print(f"Model saved at {model_path}")
-
-# Example Usage
-# save_trained_model(model, 'models/fake_news_model.h5')
